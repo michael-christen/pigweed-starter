5d5f4266b5fc4ae0de139dd211023ebf068305f9
michael-christen <mchristen96@gmail.com>
2020-03-22 13:47:30 -0700

    setup activate and bootstrap

---
+++ activate.sh
@@ -0,0 +1,4 @@
+# Setup terminal for use (assumes bootstrap.sh has already been sourced)
+
+# Just call pigweed activate
+. ./external/pigweed/repo/activate.sh
---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+# Setup environment as needed
+
+# Call pigweed boostrap
+. external/pigweed/repo/bootstrap.sh
